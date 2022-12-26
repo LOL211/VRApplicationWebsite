e33e67b5e7fb0948ed14c6f5025a28679526ec87
Kush Banbah <kbanbah@yahoo.com>
2022-12-27 01:48:25 +0800

    Reading response from server

--- script.js
+++ script.js
@@ -15,25 +15,56 @@ login_btn = document.getElementById('login_btn')
 
 loginpass = document.getElementById('login_password')
 loginemail = document.getElementById('login_email')
+loginpass.value="test1234"
+loginemail.value="t@t.com"
 
 
-const login = () => {
+
+let user;
+const getIdToken =()=> user.getIdToken().then((result) => {return result});
+const getName = async ()=>{
+    fetch("http://localhost:8080/name",
+    {
+        method:"POST",
+        headers: {
+        'Accept':"*/*",
+        'Content-Type':"application/json",
+        'Access-Control-Allow-Origin': '*'
+        },
+        body:JSON.stringify(
+        {"requestType":"NAME",
+            "idToken": await getIdToken()
+        }
+        )
+    }).then(response => {
+        d = new TextDecoder();
+      let read = response['body'].getReader();
+      read.read().then(t=>  console.log( d.decode(t.value)));  
+      
+    });
+}
+const login = async () => {
   const email = loginemail.value
   const password = loginpass.value;
 
   //Built in firebase function responsible for authentication
   auth.signInWithEmailAndPassword(email, password)
-  .then(() => {
+  .then(async (response) => {
+   
     //Signed in successfully
     alert('You\'re successfully signed in !');
+    
+    user = response["user"]
+    getName();
+
   })
   .catch(error => {
-    console.error(error);
+    alert("Unsucessful sign-in!")
   })
 }
 
 
 
 
+
 login_btn.addEventListener("click", login)
-console.log(login_btn)
