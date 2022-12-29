ed5a33d3fc4238e07e9b6973b52d88add8ba74db
Kush Banbah <kbanbah@yahoo.com>
2022-12-30 02:32:07 +0800

    Created setup.js to not repeat setup code

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,44 +1,5 @@
-const firebaseApp = firebase.initializeApp(
-    {apiKey: "AIzaSyCpefYz7bDeQkV1evWvFpuEADfNPvsuABU",
-  authDomain: "vr-application-29195.firebaseapp.com",
-  databaseURL: "https://vr-application-29195-default-rtdb.firebaseio.com",
-  projectId: "vr-application-29195",
-  storageBucket: "vr-application-29195.appspot.com",
-  messagingSenderId: "454382693464",
-  appId: "1:454382693464:web:5b34e12099989ab74dee2d"});
-  
-  const getCourses = async ()=>{
-    
- 
-    fetch("http://localhost:8080/home",
-    {
-        method:"POST",
-        headers: {
-        'Accept':"*/*",
-        'Content-Type':"application/json",
-        'Access-Control-Allow-Origin': '*'
-        },
-        body:JSON.stringify(
-        {"requestType":"HOME",
-            "idToken": getCookie("id")
-        }
-        )
-    }).then(response => {
-        if(response==null) console.log("No response");
-        textDecoder = new TextDecoder();
-      let read = response['body'].getReader();
-      read.read().then(text=>  {
-        
-        let jsonResponse = JSON.parse(textDecoder.decode(text.value));
-       setup(jsonResponse)
-        })  
-      
-    });
-}
-
 function setup(response)
-{
-  
+{  
   let courselist = JSON.parse(response.courses);
   console.log(courselist);
   
@@ -54,9 +15,6 @@ function setup(response)
   document.getElementById("title").innerHTML+=response.name;
 }
 
-
-
-
 function createcourse(coursename, teacher){
   let child = document.createElement("div");
   child.setAttribute("class", "child");
@@ -64,29 +22,10 @@ function createcourse(coursename, teacher){
   return child;
 }
 
-function createlinks(course)
-{
-  let child = document.createElement("li");
-  child.innerHTML = "<a>"+course+"</a>";
-  return child;
-}
-
-
-
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+  window.onload = async ()=>{
+    let setupmod = await import("./setup.js")
+    createlinks = setupmod.createlinks;
+    setupmod.getCourses(setup)
+    
   }
-
-getCourses();
+  
---
+++ Scripts/setup.js
@@ -0,0 +1,60 @@
+const firebaseApp = firebase.initializeApp(
+    {
+      apiKey: "AIzaSyCpefYz7bDeQkV1evWvFpuEADfNPvsuABU",
+    authDomain: "vr-application-29195.firebaseapp.com",
+    databaseURL: "https://vr-application-29195-default-rtdb.firebaseio.com",
+    projectId: "vr-application-29195",
+    storageBucket: "vr-application-29195.appspot.com",
+    messagingSenderId: "454382693464",
+    appId: "1:454382693464:web:5b34e12099989ab74dee2d"});
+    
+ export  const getCourses = async (setup)=>{
+      
+   
+      fetch("http://localhost:8080/home",
+      {
+          method:"POST",
+          headers: {
+          'Accept':"*/*",
+          'Content-Type':"application/json",
+          'Access-Control-Allow-Origin': '*'
+          },
+          body:JSON.stringify(
+          {"requestType":"HOME",
+              "idToken": getCookie("id")
+          }
+          )
+      }).then(response => {
+          if(response==null) console.log("No response");
+        let textDecoder = new TextDecoder();
+        let read = response['body'].getReader();
+        read.read().then(text=>  {
+          
+          let jsonResponse = JSON.parse(textDecoder.decode(text.value));
+         setup(jsonResponse)
+          })  
+        
+      });
+}
+
+export  function createlinks(course)
+{
+  let child = document.createElement("li");
+  child.innerHTML = "<a>"+course+"</a>";
+  return child;
+}
+
+export function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
