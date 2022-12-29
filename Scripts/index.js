49711700334fe90d52968948bbe0822ebcc445cd
Kush Banbah <kbanbah@yahoo.com>
2022-12-30 02:32:24 +0800

    removed getname function

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,3 @@
-// import { getfiles } from "./test.js";
-
 const firebaseApp = firebase.initializeApp(
 {
   apiKey: "AIzaSyCpefYz7bDeQkV1evWvFpuEADfNPvsuABU",
@@ -14,54 +12,29 @@ appId: "1:454382693464:web:5b34e12099989ab74dee2d"
 
 const auth = firebaseApp.auth();
 
-
 login_btn = document.getElementById('login_btn')
-
 loginpass = document.getElementById('login_password')
 loginemail = document.getElementById('login_email')
-
 loginpass.value="test1234";
 loginemail.value="teacher1@gmail.com";
 
 
 let user;
 const getIdToken =()=> user.getIdToken().then((result) => {return result});
-const getName = async ()=>{
-    fetch("http://localhost:8080/name",
-    {
-        method:"POST",
-        headers: {
-        'Accept':"*/*",
-        'Content-Type':"application/json",
-        'Access-Control-Allow-Origin': '*'
-        },
-        body:JSON.stringify(
-        {"requestType":"NAME",
-            "idToken": await getIdToken()
-        }
-        )
-    }).then(response => {
-        d = new TextDecoder();
-      let read = response['body'].getReader();
-      read.read().then(t=>  console.log( d.decode(t.value)));  
-      
-    });
-}
 
 const login = async () => {
   const email = loginemail.value
   const password = loginpass.value;
 
-  //Built in firebase function responsible for authentication
+
   auth.signInWithEmailAndPassword(email, password)
   .then(async (response) => {
    
-    //Signed in successfully
-    alert('You\'re successfully signed in !');
-    console.log(response["user"]);
-    user = response["user"]
-    // getName();
   
+    alert('You\'re successfully signed in !');
+
+    user = response["user"]
+
       const cookies = document.cookie.split(";");
   
       for (let i = 0; i < cookies.length; i++) {
@@ -73,9 +46,6 @@ const login = async () => {
   
     document.cookie = "id="+await getIdToken()+"; path=/;";
     document.cookie = "refreshtoken="+user.refreshToken+"; path=/";
-    // getfiles();
-    // let getfiles = await import("./test.js")
-    // getfiles.getfiles();
 
     window.open("../Html/home.html");
   })
@@ -83,13 +53,6 @@ const login = async () => {
     alert("Unsucessful sign-in!")
     console.log(error)
   })
-
-
-
 }
-
-
-
-
 
 login_btn.addEventListener("click", login)
