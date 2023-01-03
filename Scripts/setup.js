f5e483fb15d6f80fb2d70bfb45a0cd9c4195f096
Kush <kbanbah@yahoo.com>
2023-01-03 14:53:56 +0800

    Update setup.js

--- Scripts/setup.js
+++ Scripts/setup.js
@@ -8,10 +8,16 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebas
 
 
 
-export const firebaseApp = initializeApp(
-    {
-        //secret
-    });
+const firebaseApp = firebase.initializeApp(
+{
+  apiKey: "AIzaSyCpefYz7bDeQkV1evWvFpuEADfNPvsuABU",
+authDomain: "vr-application-29195.firebaseapp.com",
+databaseURL: "https://vr-application-29195-default-rtdb.firebaseio.com",
+projectId: "vr-application-29195",
+storageBucket: "vr-application-29195.appspot.com",
+messagingSenderId: "454382693464",
+appId: "1:454382693464:web:5b34e12099989ab74dee2d"
+});
     
 export const makeCourseRequest = async()=> {
   let r; 
